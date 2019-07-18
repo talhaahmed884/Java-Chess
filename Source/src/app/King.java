677c35de9e546a165f4610919b9e0ea4daed71e8
talhaahmed884 <52800026+talhaahmed884@users.noreply.github.com>
2019-07-18 13:46:11 +0500

    Add files via upload

---
+++ Source/src/app/King.java
@@ -0,0 +1,70 @@
+package app;
+
+public class King  extends Piece
+{
+	protected boolean checkmate;
+	
+	public King(int rowarg, int colarg, int colorarg) 
+	{
+		super(rowarg,colarg,colorarg);
+	}
+	
+	King(King obj)
+	{
+		super(obj);			//copy constructor for class piece
+	}
+	
+	public boolean checkMove(int rowarg, int colarg, Board board) 
+	{
+		if(rowarg>=0 && rowarg<=7 && colarg>=0 && colarg<=7)
+		{
+			if(Math.abs(row-rowarg)==1 && col==colarg || Math.abs(col-colarg)==1 && row==rowarg)
+			{
+				if(board.pieces[rowarg][colarg]==null)		//empty space
+				{
+					return true;
+				}
+				else if(board.pieces[rowarg][colarg]!=null && board.pieces[rowarg][colarg].color==board.pieces[row][col].color)			//same team piece
+				{
+					return false;
+				}
+				else if(board.pieces[rowarg][colarg]!=null && board.pieces[rowarg][colarg].color!=board.pieces[row][col].color)			//opponent team piece
+				{
+					return true;
+				}
+				else		//in case of unexpected condition/error
+				{
+					return false;
+				}
+			}
+			else		//in case of unexpected condition/error
+			{
+				return false;
+			}
+		}
+		else		//in case of any unexpected condition/error
+		{
+			return false;
+		}
+	}	
+	public void drawPiece() 				//Functionality for drawing the pieces on canvas
+	{
+		if(this.isdead)
+		{
+			System.out.print(" - ");
+		}
+		else if(this.color==0)			//0 is for Black
+		{
+			System.out.print("|k ");
+		}
+		else if(this.color==1)			// 1 is for White
+		{
+			System.out.print("|K ");
+		}
+		else
+		{
+			System.out.print(" ");		//in case of any errors
+		}	
+	}
+	
+}
